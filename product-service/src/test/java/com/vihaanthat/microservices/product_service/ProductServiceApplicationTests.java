0549ef211c15b3ce1ecc42f16ceb6515880340a9
Vihaan Thatiparthi <118706620+vihaan-that@users.noreply.github.com>
2025-10-22 19:11:43 +0530

    Add ProductService application and tests with MongoDB integration

---
+++ product-service/src/test/java/com/vihaanthat/microservices/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,56 @@
+package com.vihaanthat.microservices.product_service;
+
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ProductServiceApplicationTests {
+
+    @ServiceConnection
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+    static {
+        mongoDBContainer.start();
+    }
+	@Test
+	void shouldCreateProduct() {
+        String requestBody = """
+                {
+                    "name": "iPhone 17",
+                    "description": "Latest model of iPhone",
+                    "price": 999.99
+                }
+                """;
+
+        RestAssured.given()
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post("/api/product")
+                .then()
+                .statusCode(201)
+                .body("id", Matchers.notNullValue())
+                .body("name", Matchers.equalTo("iPhone 17"))
+                .body("description", Matchers.equalTo("Latest model of iPhone"))
+                .body("price", Matchers.equalTo(999.99f));
+
+
+
+	}
+
+}
